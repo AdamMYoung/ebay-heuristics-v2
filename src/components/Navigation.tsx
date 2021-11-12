0b5784a851bb11f02b67132cd4c4b9cfa0b28047
Adam Young <adamyoungg97@gmail.com>
2021-11-12 15:55:20 +0000

    Improved details

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,36 @@
-import { Stack, Heading, Button, Text, Spinner, HStack, useColorMode } from '@chakra-ui/react';
+import { Stack, Heading, Button, Text, Spinner, HStack, useColorMode, Divider } from '@chakra-ui/react';
+import { useMemo, useState } from 'react';
 
 import { useEbay } from '../providers/EbayProvider';
 import { useOrders } from '../providers/OrderProvider';
 
 export const Navigation = () => {
+	const [hideDetails, setHideDetails] = useState(false);
 	const { colorMode } = useColorMode()
 	const { isAuthenticated, login } = useEbay();
 	const { isLoading, orders } = useOrders()
+
+	const amountEarned = orders.reduce((acc, order) => acc + order.itemsOrdered.reduce((list, item) => list + parseFloat(item.cost), 0), 0)
+
+	const itemsSold = useMemo(() => orders.reduce<string[]>((acc, order) => {
+		const items = order.itemsOrdered.map(i => i.title)
+
+		acc.push(...items)
+		return acc
+	}, []), [orders])
+
+	const groupedItems = useMemo(() => {
+		return itemsSold.reduce((prev, item) => {
+			if (item in prev) {
+				prev[item]++;
+			}
+			else {
+				prev[item] = 1;
+			}
+			return prev;
+		}, {} as { [key: string]: number })
+	}, [itemsSold])
+
 
 	return (
 		<Stack
@@ -22,7 +46,16 @@ export const Navigation = () => {
 			<Button disabled={isAuthenticated} onClick={login}>{!isAuthenticated ? "Login to eBay" : "Logged in"}</Button>
 
 			{isAuthenticated && isLoading && <HStack><Spinner /><Text>Loading order data...</Text></HStack>}
-			{isAuthenticated && !isLoading && <Text>Loaded {orders.length} orders</Text>}
+			{!hideDetails && isAuthenticated && !isLoading && <Stack spacing="2">
+				<Heading size="md" fontWeight="bold">Stats</Heading>
+				<Text>Over the past 90 days, you have:</Text>
+				<Text>Had <b>{orders.length}</b> orders</Text>
+				<Text>Earned <b>£${amountEarned}</b></Text>
+				<Divider />
+				<Text>Breakdown:</Text>
+				{Object.keys(groupedItems).map(key => <Text>Sold ${groupedItems[key]} of ${key}</Text>)}
+			</Stack>}
+			<Button onClick={() => setHideDetails(visible => !visible)}>{hideDetails ? "Show" : "Hide"} details</Button>
 		</Stack>
 	);
 };
